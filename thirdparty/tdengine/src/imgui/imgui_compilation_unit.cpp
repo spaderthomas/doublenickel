1fc3840d68373c4680fe1f317e188ccff2d1b8d5
Thomas Spader <thomas.spader@gmail.com>
2024-11-21 23:34:43 -0500

    fixup the build

--- thirdparty/tdengine/src/imgui/imgui_compilation_unit.cpp
+++ thirdparty/tdengine/src/imgui/imgui_compilation_unit.cpp
@@ -1,12 +1,13 @@
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "imgui/imgui_impl_opengl3.h"
+// If __GL_H__ is not defined, then the Windows OpenGL header will define GL_VERSION_1_1. For some
+// reason, the Windows header does not define the macro version of the function pointers, so the
+// ImGui OpenGL loader will fail. Define this here to prevent that.
+#define __GL_H__
 
 #include "cimgui.cpp"
 #include "imgui.cpp"
 #include "imgui_demo.cpp"
 #include "imgui_draw.cpp"
-#include "imgui_impl_glfw.cpp"
-#include "imgui_impl_opengl3.cpp"
 #include "imgui_tables.cpp"
 #include "imgui_widgets.cpp"
+#include "imgui_impl_glfw.cpp"
+#include "imgui_impl_opengl3.cpp"
